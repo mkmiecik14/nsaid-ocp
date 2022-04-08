763dcba9d3ec81f61d69a21df167d2c77f85a0ef
Matt Kmiecik <mkmiecik14@gmail.com>
2022-04-08 16:26:49 -0500

    progress on scripts

--- prepro-api-calls.R
+++ prepro-api-calls.R
@@ -22,6 +22,7 @@ load("../output/nsaid-baselinevisit-api.rda") # NSAID baseline visit
 load("../output/nsaid-followupvisit-api.rda") # NSAID follow-up visit
 
 # Combining data for efficiency
+# OCP DATA
 ocp_avisit_data <- 
   bind_rows(arm1_avisit1_api, arm1_avisit2_api, arm1_avisit3_api) %>%
   mutate(
@@ -32,6 +33,17 @@ ocp_avisit_data <-
     )
     ) %>%
   rename(visit_month = event_id)
+
+# NSAID DATA
+nsaid_avisit_data <-
+  bind_rows(nsaid_baselinevisit_api, nsaid_followupvisit_api) %>%
+  mutate(
+    event_id = case_when(
+      event_id == "baseline_visit_arm_1" ~ 0,
+      event_id == "follow_up_visit_arm_1" ~ 6,
+    )
+  ) %>%
+  rename(visit_month = event_id)
   
 
 ################
@@ -39,6 +51,8 @@ ocp_avisit_data <-
 # Bladder Test #
 #              #
 ################
+
+# OCP bladder data
 ocp_bladder_data <- 
   ocp_avisit_data %>%
   filter(grepl("bt", field_name) | grepl("bladder", field_name)) %>%
@@ -46,7 +60,11 @@ ocp_bladder_data <-
     id_cols = c(record, visit_month), 
     names_from = field_name, 
     values_from = value
-    )
+    ) %>%
+  select(-ic2e_probtotal) # removes column accidentally grabbed in grepl
+
+# NSAID bladder data
+nsaid_avisit_data %>% # do similar steps as above
   
 # NEXT STEPS:
 # Save the bladder data out and analyze in analysis script
