6189a7a58975193fbeea4140548424ee409a9f84
Matt Kmiecik <mkmiecik14@gmail.com>
2022-06-02 17:05:57 -0500

    added prepro script for ppts

--- prepro-ppt.R
+++ prepro-ppt.R
@@ -7,6 +7,8 @@
 
 source("r-prep.R") # Prepare R workspace
 
+load("../output/ss-masterlist.rda")
+
 ############
 #          #
 # OCP Data #
@@ -14,8 +16,12 @@ source("r-prep.R") # Prepare R workspace
 ############
 
 # Loads in data
-ppt_data <- 
-  read_excel(path = "../data/ppt.xlsx", sheet = "matt-prep", na = c("MS", "ms")) %>%
+ocp_ppt_data <- 
+  read_excel(
+    path = "../data/ocp-ppt-data.xlsx", 
+    sheet = "matt-prep", 
+    na = c("MS", "ms")
+    ) %>%
   separate(ss, into = c("ss", "session")) %>% # some ss were named with visit
   mutate(
     ss = as.numeric(ss),
@@ -131,10 +137,109 @@ ppt_data <-
   ) %>%
   
   # stopped data editing after ss 49, decided to visualize data for issues
-  mutate(force = ifelse(force == 0, NA, force)) # force cannot be equal to zero
-  
-# Saving out data ----
-save(ppt_data, file = "../output/ppt-data.RData") # RData
-write_csv(ppt_data, file = "../output/ppt-data.csv") # CSV 
+  mutate(force = ifelse(force == 0, NA, force)) %>% # force cannot be equal to zero
+  rename(ppt_notes = notes) %>% # renames for merging with ss_masterlist
+  # puts visit in terms of months
+  mutate(
+    visit_month = 
+      case_when(
+        visit == 1 ~ 0,
+        visit == 2 ~ 6,
+        visit == 3 ~ 12,
+        is.na(visit) ~ NA_real_
+      )
+  ) %>%
+  relocate(visit_month, .before = visit) %>%
+  select(-visit)
 
-rm(ppt_data) # cleans script object(s)
+
+###############
+#             #
+# NSAID Data  #
+#             #
+###############
+
+# Loads in data
+nsaid_ppt_data <- 
+  read_excel(
+    path = "../data/nsaid-ppt-data.xlsx", 
+    sheet = "matt-prep", 
+    na = c("MS", "ms")
+  ) %>%
+  mutate(test = gsub(" ", "_", test)) %>% # removes space after PPTs
+  mutate(force = gsub("\\*", "_*", force)) %>% # this is to handle the *
+  separate(force, into = c("force", "force_notes"), sep = "_") %>%
+  mutate(
+    force = as.numeric(force), 
+    rate = as.numeric(rate),
+    duration = as.numeric(duration),
+    visit_month = 
+      case_when(
+        visit == 1 ~ 0, # baseline
+        visit == 2 ~ 6  # 6 month follow-up visit
+        )
+    ) %>%
+  relocate(visit_month, .before = visit) %>%
+  select(-visit) %>%
+  rename(ppt_notes = notes)
+
+######################
+#                    #
+# Combining all data #
+#                    #
+######################
+
+# Loads masterlist
+load("../output/ss-masterlist.rda")
+
+# Prepares data
+# NSAID ss only masterlist
+nsaid_masterlist <- 
+  ss_masterlist %>% 
+  select(global_id, nsaid_id, nsaid_r, group, drug, notes) %>%
+  filter(drug == "NSAID")
+
+# OCP ss only masterlist
+ocp_masterlist <-
+  ss_masterlist %>%
+  select(global_id:shortannualsr, group:notes) %>%
+  filter(drug %in% c("OCP_NO", "OCP_CONT", "OCP_CYCL"))
+
+# combines nsaid ppt data with ss info
+nsaid_ppt_ss <- 
+  left_join(
+    nsaid_masterlist,
+    nsaid_ppt_data,
+    by = c("nsaid_id" = "ss")
+  )
+
+# combines ocp ppt data with ss info
+ocp_ppt_ss <- 
+  left_join(
+    ocp_masterlist,
+    ocp_ppt_data,
+    by = c("ss" = "ss")
+  )
+
+# Combines all bladder data together
+ppt_data <- 
+  bind_rows(
+    nsaid_ppt_ss %>% select(-nsaid_id, -nsaid_r),
+    ocp_ppt_ss %>% select(-ss, -arm1r, -shortannualsr)
+  )
+
+# Saving out data
+save(ppt_data, file = "../output/ppt-data.rda") # RData
+write_csv(ppt_data, file = "../output/ppt-data.csv") # CSV
+
+# cleans script object(s)
+rm(
+  ocp_ppt_data, 
+  nsaid_ppt_data, 
+  nsaid_masterlist, 
+  nsaid_ppt_ss, 
+  ocp_masterlist,
+  ocp_ppt_ss,
+  ppt_data,
+  ss_masterlist
+  ) 
